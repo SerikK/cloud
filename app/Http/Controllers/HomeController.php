eaecc2789c1b631c7a129e3182982e1d5702886c
Админ <kenzhegalin.serik@gmail.com>
2020-04-22 12:21:44 +0500

    Laratrust added Form to upload file for administrator role

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\UserFile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -26,6 +28,26 @@ class HomeController extends Controller
     public function index()
     {
         $files = Storage::disk('public')->files('user-files/' . Auth::id());
-        return view('home', compact('files'));
+        $users = User::all();
+        return view('home', compact('files', 'users'));
+    }
+
+    public function upload(Request $request)
+    {
+        $files = $request->file('files');
+        $userId = $request->user_id;
+
+        if($request->hasFile('files'))
+        {
+            foreach ($files as $file) {
+                $filename = $file->store('user-files/' . $userId .'/', ['disk' => 'public']);
+                UserFile::create([
+                    'user_id' => $userId,
+                    'file' => $filename
+                ]);
+            }
+        }
+
+        return redirect()->back();
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,34 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @foreach($files as $file)
-                        <p>
-                        <a href="/storage/{{ $file }}" target="_blank">
-                            <span class="oi oi-file"></span>
-                            <span>{{ $file }}</span>
-                        </a>
-                        </p>
-                    @endforeach
+                    @if (Auth::user()->hasRole('administrator'))
+                        <h4>Add user file</h4>
+                        <form action="{{ route('upload') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">
+                                <label>User</label>
+                                <select name="user_id" class="form-control">
+                                    @foreach($users as $user)
+                                        <option value="{{ $user->id }}">{{ $user->name .' '. $user->email }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>File</label>
+                                <input type="file" name="files[]" multiple class="form-control-file">
+                            </div>
+                            <button class="btn btn-primary" type="submit">Отправить</button>
+                        </form>
+                    @else
+                        @foreach($files as $file)
+                            <p>
+                                <a href="/storage/{{ $file }}" target="_blank">
+                                    <span class="oi oi-file"></span>
+                                    <span>{{ $file }}</span>
+                                </a>
+                            </p>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
