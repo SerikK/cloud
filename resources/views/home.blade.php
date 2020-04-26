a25f76a8cabc7b0191a3bb8043cfad8c1e7d962f
Админ <kenzhegalin.serik@gmail.com>
2020-04-26 10:58:01 +0500

    S3 storage fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
                     @else
                         @foreach($files as $file)
                             <p>
-                                <a href="/storage/{{ $file }}" target="_blank">
+                                <a href="{{ Storage::disk()->url($file) }}" target="_blank">
                                     <span class="oi oi-file"></span>
                                     <span>{{ $file }}</span>
                                 </a>
